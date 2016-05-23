5e86ce5bfec41885a15d7d5fac548633fbc195c0
Marko Simunac <MSimunac3@gatech.edu>
2016-05-22 21:10:58 -0600

    animation

--- app/src/main/java/poloapps/scarequitsmoking/MainActivity.java
+++ app/src/main/java/poloapps/scarequitsmoking/MainActivity.java
@@ -1,7 +1,9 @@
 package poloapps.scarequitsmoking;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//uy
+        ImageView mImageViewFilling = (ImageView) findViewById(R.id.MainAnim);
+        ((AnimationDrawable) mImageViewFilling.getBackground()).start();
+
+    }
 }
