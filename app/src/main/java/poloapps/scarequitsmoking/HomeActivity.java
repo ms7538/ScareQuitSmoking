f1d61dc65d1dce55dfa2aabc6206aabd98257269
Marko <ms7538@gmail.com>
2016-12-17 22:52:48 -0700

    text values and calculations added

--- app/src/main/java/poloapps/scarequitsmoking/HomeActivity.java
+++ app/src/main/java/poloapps/scarequitsmoking/HomeActivity.java
@@ -17,30 +17,66 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home2);
+
         ImageView mImageViewFilling = (ImageView) findViewById(R.id.MainAnim);
         ((AnimationDrawable) mImageViewFilling.getBackground()).start();
-        final MediaPlayer Flatline= MediaPlayer.create(this, R.raw.flatline);
+
+        //final MediaPlayer Flatline= MediaPlayer.create(this, R.raw.flatline);
         // Flatline.start();
-        int maxworldwideTD= 6000000;
-        int maxusTD=500000;
-        Calendar calendar = Calendar.getInstance();
-        int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        // Flatline.stop();
+        setdeaths();
 
-        TextView usHourly = (TextView) findViewById(R.id.usH);
-        TextView usMonthly = (TextView) findViewById(R.id.usM);
-        TextView usYearly = (TextView) findViewById(R.id.usY);
-        TextView wwHourly = (TextView) findViewById(R.id.wwH);
-        TextView wwMonthly = (TextView) findViewById(R.id.wwM);
-        TextView wwYearly = (TextView) findViewById(R.id.wwY);
-
-        usHourly.setText(Integer.toString(dayOfYear));
-        usMonthly.setText(Integer.toString(hourOfDay));
-        // Button button = (Button) findViewById(R.id.simple);
-
-        //button.setOnClickListener(new View.OnClickListener() {
-            //@Override
-           // public void onClick(View arg0) {
-              //  Flatline.stop();
+        Button button = (Button) findViewById(R.id.simple);
+            button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                setdeaths();
             }
+        });
+    }
+    private void setdeaths( ){
+
+        int WWdeathsperday  = 16854;
+        int USdeathsperday  = 1440;
+        int USdeathsperhour = USdeathsperday / 24;
+        int WWdeathsperhour = WWdeathsperday / 24;
+        int WWdeathspermin  = WWdeathsperhour / 60;
+
+        Calendar calendar = Calendar.getInstance();
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int dayOfYear  = calendar.get(Calendar.DAY_OF_YEAR);
+        int hourOfDay  = calendar.get(Calendar.HOUR_OF_DAY);
+        int minOfHour  = calendar.get(Calendar.MINUTE);
+
+        int hourlyUS   = minOfHour; // 1 death per minute
+        int hourlyWW   = minOfHour * WWdeathspermin;
+        int dailyUS    = ( hourOfDay * USdeathsperhour )+ minOfHour ;
+        int monthUS    = ( dayOfMonth - 1 ) * USdeathsperday + dailyUS;
+        int dailyWW    = ( hourOfDay * WWdeathsperhour )+ minOfHour ;
+        int monthWW    = ( dayOfMonth - 1 ) * WWdeathsperday + dailyWW;
+        int yearUS     = ( dayOfYear - 1 ) * USdeathsperday + dailyUS;
+        int yearWW     = ( dayOfYear - 1 ) * WWdeathsperday + dailyWW;
+
+        TextView usHourly  = (TextView) findViewById(R.id.usH);
+        TextView usMonthly = (TextView) findViewById(R.id.usM);
+        TextView usYearly  = (TextView) findViewById(R.id.usY);
+        TextView usToday   = (TextView) findViewById(R.id.usT);
+        TextView wwHourly  = (TextView) findViewById(R.id.wwH);
+        TextView wwMonthly = (TextView) findViewById(R.id.wwM);
+        TextView wwYearly  = (TextView) findViewById(R.id.wwY);
+        TextView wwToday   = (TextView) findViewById(R.id.wwT);
+
+        usHourly.setText(Integer.toString(hourlyUS));
+        usToday.setText(Integer.toString(dailyUS));
+        usMonthly.setText(Integer.toString(monthUS));
+        usYearly.setText(Integer.toString(yearUS));
+        wwHourly.setText(Integer.toString(hourlyWW));
+        wwToday.setText(Integer.toString(dailyWW));
+        wwMonthly.setText(Integer.toString(monthWW));
+        wwYearly.setText(Integer.toString(yearWW));
+
+
+
+   }
+
 }
