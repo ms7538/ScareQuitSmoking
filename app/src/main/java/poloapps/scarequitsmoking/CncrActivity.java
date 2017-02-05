9916d6433c9d7f80a534536105804a15a6558c12
Marko <ms7538@gmail.com>
2017-02-04 20:18:44 -0700

    cancer layout and activity  updated 3

--- app/src/main/java/poloapps/scarequitsmoking/CncrActivity.java
+++ app/src/main/java/poloapps/scarequitsmoking/CncrActivity.java
@@ -1,6 +1,7 @@
 package poloapps.scarequitsmoking;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -32,21 +33,37 @@ public class CncrActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        Spinner spinner1 = (Spinner) findViewById(R.id.cignum_sel_spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.cignum_sel_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.freq_sel_spinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.freq_sel_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.leng_sel_spinner);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.len_sel_array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
-        //final TextView cancer_tv = (TextView) findViewById(R.id.cancer_TV);
-        //final EditText et  = (EditText) findViewById(R.id.editText);
-        //final TextView ctv = (TextView) findViewById(R.id.edittext_val);
-        //ctv.setText(String.valueOf(0));
+        SharedPreferences mSettings = this.getSharedPreferences("Settings", 0);
+        final SharedPreferences.Editor editor = mSettings.edit();
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapter, View v,
                                        int position, long id) {
                 // On selecting a spinner item
-                 String spinval = adapter.getItemAtPosition(position).toString();
-          //      cancer_tv.setText(spinval);
+                String spinval = adapter.getItemAtPosition(position).toString();
+                //      cancer_tv.setText(spinval);
                 // Showing selected spinner item
                 Toast.makeText(getApplicationContext(),
                         spinval, Toast.LENGTH_SHORT).show();
@@ -56,29 +73,82 @@ public class CncrActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
             }
         });
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter1, View v,
+                                       int position, long id) {
+                // On selecting a spinner item
+                String spinval1 = adapter1.getItemAtPosition(position).toString();
+                Button AdH = (Button) findViewById(R.id.button2);
+                if (spinval1.contains("Select")) {
+                    editor.putInt("S1", 0);
+                }
+                else{
+                    editor.putInt("S1", 1);
+                }
+                editor.commit();
+                AdH.setEnabled(checkAH());
 
-       // Button B14 = (Button) findViewById(R.id.button14);
-        //B14.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-            //public void onClick(View arg0) {
-               //final int number =Integer.parseInt(et.getText().toString());
-                //ctv.setText(String.valueOf(number));
-         //}
-        //});
-        //final MediaPlayer Flatline= MediaPlayer.create(this, R.raw.flatline);
-        // Flatline.start();
-        // Flatline.stop();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter2, View v,
+                                       int position, long id) {
+                // On selecting a spinner item
+                String spinval2 = adapter2.getItemAtPosition(position).toString();
+                //      cancer_tv.setText(spinval);
+                Button AdH = (Button) findViewById(R.id.button2);
+                if (spinval2.contains("Select")) {
+                    editor.putInt("S2", 0);
+                }
+                else{
+                    editor.putInt("S2", 1);
+                }
+                editor.commit();
+                AdH.setEnabled(checkAH());
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter3, View v,
+                                       int position, long id) {
+                // On selecting a spinner item
+                String spinval3 = adapter3.getItemAtPosition(position).toString();
+
+                Button AdH = (Button) findViewById(R.id.button2);
+                if (spinval3.contains("Select")) {
+                    editor.putInt("S3", 0);
+                }
+                else{
+                    editor.putInt("S3", 1);
+                }
+                editor.commit();
+                AdH.setEnabled(checkAH());
+                // Showing selected spinner item
+               // Toast.makeText(getApplicationContext(),
+                 //       spinval3, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+
 
         //setRepeatingAsyncTask();
-//        Button AsthmaButton = (Button) findViewById(R.id.Aradiobutton);
-//        AsthmaButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View arg0) {
-//
-//                Toast.makeText(getApplicationContext(),"Asthma Clicked", Toast.LENGTH_LONG).show();
-//            }
-//        });
-//
 //        Button LungCancerButton = (Button) findViewById(R.id.LCradiobutton);
 //        LungCancerButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -99,6 +169,7 @@ public class CncrActivity extends AppCompatActivity {
         super.startActivity(intent);
         return;
     }
+
     private void setRepeatingAsyncTask() {
 
         final Handler handler = new Handler();
@@ -119,6 +190,17 @@ public class CncrActivity extends AppCompatActivity {
             }
         };
         timer.schedule(task, 0, 30*1000);  // interval of one minute
+    }
+
+    private boolean checkAH(){
+        SharedPreferences mSettings = this.getSharedPreferences("Settings", 0);
+        Integer Sp1Sel = mSettings.getInt("S1", 0);
+        Integer Sp2Sel = mSettings.getInt("S2", 0);
+        Integer Sp3Sel = mSettings.getInt("S3", 0);
+        if (Sp1Sel == 0 || Sp2Sel == 0 || Sp3Sel == 0) {
+            return false;
+        }
+        else return true;
     }
 
 }
